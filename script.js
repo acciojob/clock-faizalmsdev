13fce16f10c8161c65c2e9da43da8c0c2b854b3b
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-20 19:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+function updateDateTime() {
+  var currentDate = new Date();
+
+  var year = currentDate.getFullYear();
+  var month = formatDateTimeComponent(currentDate.getMonth() + 1);
+  var day = formatDateTimeComponent(currentDate.getDate());
+
+  var hours = formatDateTimeComponent(currentDate.getHours());
+  var minutes = formatDateTimeComponent(currentDate.getMinutes());
+  var seconds = formatDateTimeComponent(currentDate.getSeconds());
+
+  var dateTimeString = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("datetime").textContent = dateTimeString;
+}
+
+function formatDateTimeComponent(component) {
+  if (component < 10) {
+    return "0" + component;
+  }
+  return component;
+}
+
+// Call updateDateTime() initially to display the current date and time
+updateDateTime();
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
